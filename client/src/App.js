4b8314be21dac0351037b21c4e1c4dd725e2b37c
Viet Vo <viet.allen.qvo@gmail.com>
2019-05-16 23:40:30 -0700

    passed props to Home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 // MAIN APP FILE
 // Dependencies and associated COMPONENTS
 import React, { Component } from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Grid } from '@material-ui/core';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import { createMuiTheme } from '@material-ui/core/styles';
 // import Grid from '@material-ui/core/Grid';
 import { grey } from '@material-ui/core/colors';
+
+import Home from './pages/Home';
 
 // import API from './utils/test';
 const theme = createMuiTheme({
@@ -18,20 +20,26 @@ const theme = createMuiTheme({
 
 class App extends Component {
   state = {
-    data: []
-  };
-  
-  // componentDidMount() {
-  //   API.getTestData()
-  //     .then(res => this.setState({data: res.data }))
-  //       .catch(err => console.log(err));
-  // }
+    data: [],
+    string: "test"
+   };
+
+  handleUpdate (updateProps) {
+    this.setState({ string: updateProps});
+  }
 
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        {/* <AppBar /> */}
-        {/* <h2>{this.state.data.test}</h2>      */}
+        <Grid>
+          <Router>
+            <Switch>
+              <Route path="/" render={() => <Home {...this.state} onUpdate={this.handleUpdate.bind(this)} />} />} />
+              {/* <Route path="/" exact component={Home} /> */}
+              {/* <Route path="/" exact component={Home} /> */}
+            </Switch>
+          </Router>
+        </Grid>
       </MuiThemeProvider>
     )
   };
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+import NavBar from '../../components/NavBar';
+import { Grid } from '@material-ui/core';
+import Typography from "@material-ui/core/Typography";
 
 export class Home extends Component {
+
+  componentDidMount() {
+    console.log(this.props)
+  } 
   render() {
     return (
       <div>
-        Home Component
+        <NavBar />
+        <Grid 
+          container
+          spacing={0}
+          direction="row"
+          alignItems="center"
+          justify="center"
+          >
+          <Paper>
+            <Typography variant="h5" component="h2" style={{margin: "1em"}}>
+              Welcome to your Home Page
+            </Typography>
+          </Paper>
+        </Grid>
       </div>
     )
-  }
+  };
 };
 
-export default Home
+export default Home;
