065fe45ad5c347ab9aeabfd6a184458996a1a88c
Viet Vo <viet.allen.qvo@gmail.com>
2019-05-18 23:01:59 -0700

    added left column cards, made with function component

--- client/src/components/GridCard/GridCard.js
+++ client/src/components/GridCard/GridCard.js
@@ -2,34 +2,56 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
-    card: {
-      minWidth: 275,
+  card: {
+    minWidth: 275,
     },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-  };
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
 
 function GridCard(props) {
+  const { classes, header, body, footer, id } = props;
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Card className={classes.card} key={id}> 
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          {header}
+        </Typography>
+      </CardContent>
+      <CardContent>
+        <Typography className={classes.pos} color="textSecondary" gutterBottom>
+          {body}
+        </Typography>
+      </CardContent>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          {footer}
+        </Typography>
+      </CardContent>
+      <CardContent>
+        <Typography className={classes.pos} color="textSecondary" gutterBottom>
+          {id}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default GridCard
+GridCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(GridCard);
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,29 +2,89 @@ import React, { Component } from 'react';
 import Paper from '@material-ui/core/Paper';
 import NavBar from '../../components/NavBar';
 import { Grid } from '@material-ui/core';
-import Typography from "@material-ui/core/Typography";
+import Typography from '@material-ui/core/Typography';
+import GridCard from '../../components/GridCard';
 
 export class Home extends Component {
-
+  state = {
+    data: [],
+    cardProp: [
+      {
+        header: "Allen",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam voluptate natus et dolorem molestias praesentium dicta explicabo illum voluptatibus, totam iste laudantium numquam accusantium quaerat debitis voluptatem quis magni odio.",
+        footer: "This is the information footer",
+        id: 0,
+      },
+      {
+        header: "Brian",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa cumque itaque pariatur corrupti perspiciatis enim, impedit ducimus autem expedita debitis, amet nemo doloribus sequi eaque aliquid? Necessitatibus explicabo voluptates hic!",
+        footer: "This is the information footer",
+        id: 1,
+      },
+      {
+        header: "Pat",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam rem at quia cum alias distinctio officiis debitis, veritatis reiciendis nulla tempore molestiae? Architecto dolorum, tempore provident ut possimus at dolores.",
+        footer: "This is the information footer",
+        id: 2,
+      },
+    ]
+  }
   componentDidMount() {
     console.log(this.props)
   } 
   render() {
+    const { cardProp } = this.state;
     return (
       <div>
         <NavBar />
-        <Grid 
+        <Grid
           container
-          spacing={0}
           direction="row"
           alignItems="center"
           justify="center"
+        >
+          <Grid
+            item
+            xs={3}
           >
-          <Paper>
-            <Typography variant="h5" component="h2" style={{margin: "1em"}}>
-              Welcome to your Home Page
-            </Typography>
-          </Paper>
+            <Grid 
+              container
+              spacing={0}
+              direction="column"
+              alignItems="center"
+              justify="center"
+              >
+                {cardProp.map(index => {
+                  console.log(index)
+                  return(<GridCard {...index} key={index.id}/>)
+                })}
+              </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={6}
+          >
+            <Grid 
+              container
+              spacing={0}
+              direction="row"
+              alignItems="center"
+              justify="center"
+              >
+              <Paper>
+                <Typography variant="h5" component="h2" style={{margin: "1em"}}>
+                  Welcome to your Home Page
+                </Typography>
+              </Paper>
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={3}
+          >
+
+          </Grid>
+            
         </Grid>
       </div>
     )
