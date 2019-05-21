9fea7efd50ab24c69e5c3ad278cc9ee8436c0475
Viet Vo <viet.allen.qvo@gmail.com>
2019-05-20 23:40:30 -0700

    routes comprehended, TODO: create controller, then Mongoose model

---
+++ server/routes/userRoutes/userRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+// controller path
+
+router.route("/a")
+    .get(function(req, res) {
+        res.json({test: "the subroute's test passed"})
+    })
+
+module.exports = router;
