2a1a284b222a504b517bb6802eab632f917ec593
EC2 Default User <ec2-user@ip-172-31-2-127.us-west-2.compute.internal>
2019-05-24 04:11:20 +0000

    fixed controller path in routes

--- server/routes/userRoutes/userRoutes.js
+++ server/routes/userRoutes/userRoutes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
-const userController = require('../../controllers');
+const userController = require('../../controllers/controller');
 
 router.route("/a")
-    .post(userController.create);
+    .get(function(req, res) {
+        res.json({test: "this get request from user/a works"})
+    })
+    .post(userController.userCreate);
 
 module.exports = router;
