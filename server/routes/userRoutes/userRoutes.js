37beb40239dc9381b136d820a4c02fd525c51bfa
Viet Vo <viet.allen.qvo@gmail.com>
2019-06-04 00:35:55 -0700

    added subroutes

--- server/routes/userRoutes/userRoutes.js
+++ server/routes/userRoutes/userRoutes.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
 const userController = require('../../controllers/controller');
 
+router.route("/")
+    .get(userController.userLoggedIn);
+
 router.route("/newUser")
-    .get(function(req, res) {
-        res.json({test: "this get request from user/a works"})
-    })
     .post(userController.userCreate);
+
+router.route("/checkLogin")
+    .post(userController.checkLogin);
+
+router.route("/logout")
+    .post(userController.userLoggedOut);
 
 module.exports = router;
